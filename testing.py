8bac78fb9254e7a3490b3b9b3b13330a99ce3858
Eric M <ern.martin12@gmail.com>
2024-03-30 10:27:12 -0400

    testing

--- testing.py
+++ testing.py
@@ -29,39 +29,53 @@ def click_button_with_css(driver, css_selector):
   )
   element.click()
 
-def navigate_to_followers(driver):
+def __main__():
+  # configure web driver
+  driver = webdriver.Chrome()
+  driver.get('https://www.instagram.com/accounts/login/')
+
+  # log into instagram
+  time.sleep(1)
+  login(driver)
+
+  # navigate to profile
   profile_css = "[href*=\"" + username + "\"]"
   click_button_with_css(driver, profile_css)
 
-def __main__():
-  driver = webdriver.Chrome()
-  driver.get('https://www.instagram.com/accounts/login/')
-  time.sleep(1)
-  login(driver)
-  navigate_to_followers(driver)
-
-  followers_css = "[href*=\"" + username + "/followers/\"]"
-  total_follower_count = WebDriverWait(driver, 10).until(
+  # navigate to followers
+  followers_css =  "[href*=\"" + username + "/followers/\"]"
+  total_follower_count = WebDriverWait(driver, 5).until(
     EC.element_to_be_clickable((By.CSS_SELECTOR, "[href*=\"" + username + "/followers/\"]"))
   )
   print(total_follower_count.text + " total followers.")
-
-  css_select_close = '[aria-label="Close"]'
-
-  following_css =  "[href*=\"" + username + "/following/\"]"
-  total_following_count = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.CSS_SELECTOR, "[href*=\"" + username + "/following/\"]"))
-  )
-  print(total_following_count.text + " total following.")
-
   click_button_with_css(driver, followers_css)
-  followers_list = get_usernames_from_dialog(driver, total_follower_count)
+  time.sleep(3)
+  # dialog_box = WebDriverWait(driver, 5).until(
+  #   EC.presence_of_element_located((By.XPATH, '/html/body/div[7]/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/div[1]/div'))
+  # )
+  dialog_box = driver.find_element(By.XPATH, "//div[@class='_aano']")
+  driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", dialog_box)
 
+  # Wait for some time to let the new content load (you may adjust the time accordingly)
+  driver.implicitly_wait(10)
+  # followers_list = get_usernames_from_dialog(driver, total_follower_count)
+
+
+  # # navigate to following
+  # css_select_close = WebDriverWait(driver, 10).until(
+  #   EC.element_to_be_clickable((By.CSS_SELECTOR, '[aria-label="Close"]'))
+  # )
   # click_button_with_css(driver, css_select_close)
-  # time.sleep(1)
 
+  # following_css =  WebDriverWait(driver, 10).until(
+  #   EC.element_to_be_clickable((By.CSS_SELECTOR, "[href*=\"" + username + "/following/\"]"))
+  # )
+  # total_following_count = following_css.text
+  # print(total_following_count + " total following.")
   # click_button_with_css(driver, following_css)
   # following_list = get_usernames_from_dialog(driver, total_following_count)
+
+  # time.sleep(1)
 
   # no_followbacks = no_followback(followers_list, following_list)
   # for i in no_followbacks:
@@ -78,29 +92,8 @@ def no_followback(followers, following):
       no_followback_list += [following[i]]
   return no_followback_list
 
-def get_usernames_from_dialog(driver, end_count):
-  # list_xpath = "//div[@role='dialog']//li"
-  # print("attempting EC xpath")
-
-  # time.sleep(3)
-  # # scroll_down(driver)
-
-  # class_name = "x1dm5mii"
-  # list_elems = driver.find_elements(By.CLASS_NAME, class_name)
-  # time.sleep(1)
-
+def get_usernames_from_dialog(driver):
   scroll_within_dialog_until_no_more_elements(driver)
-
-  # users = []
-  # for i in range(len(list_elems)):
-  #   try:
-  #     row_text = list_elems[i].text
-  #     username = row_text[0:row_text.index("\n")]
-  #     users += [username]
-  #   except:
-  #     print("continue")
-
-  # return users
 
 def check_difference_in_count(driver):
   global count
